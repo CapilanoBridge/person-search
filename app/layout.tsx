17d79945ac2c8f950d95ed8b55552fdefa921b5c
Dylan <hakirkaba@gmail.com>
2026-04-16 17:10:18 +0800

    Fix layout.tsx: clean up formatting and remove suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
     >          
